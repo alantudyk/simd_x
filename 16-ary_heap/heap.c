00b14329a0e01aafd54967426ddd936419d4b00c
Reznov <yann.collet.is.not.a.perfectionist@gmail.com>
2024-04-18 22:14:49 +0300

    >>

--- 16-ary_heap/heap.c
+++ 16-ary_heap/heap.c
@@ -1,1 +1,45 @@
 #include <immintrin.h>
+#include "heap.h"
+
+bool minq_init(minq_t *const q, size_t c) {
+    if ((q->_c = c) == 0)
+        return true;
+    c = (c + 14) / 16 + 1;
+    if ((q->_a = malloc(c * 64)) == NULL)
+        return true;
+    q->_a += 15, q->_z = 0;
+    __m256i s = _mm256_set1_epi32(q->_a[0] = INT32_MAX);
+    for (int *p = q->_a + 1, *const P = p + (q->_c - 1); p < P; p += 8)
+        _mm256_storeu_si256((void *)p, s);
+    return false;
+}
+
+void minq_release(minq_t *const q) { free(q->_a - 15), *q = (minq_t){}; }
+
+static bool sink(minq_t *const q, size_t i) {
+
+    const __m256i P2 = _mm256_set_epi32(6, 7, 4, 5, 2, 3, 0, 1),
+                  P4 = _mm256_set_epi32(4, 4, 6, 6, 0, 0, 2, 2),
+                  P8 = _mm256_set_epi32(0, 0, 0, 0, 4, 4, 4, 4);
+
+    int x[16] = {
+         7, 2, 9, 5, 4, 5, 2, 2,
+         7, 8, 7, 3, 7, 9, 9, 0,
+    };
+
+    const __m256i a = _mm256_loadu_si256((void *)x),
+                  b = _mm256_loadu_si256((void *)(x + 8));
+
+    __m256i m = _mm256_min_epi32(a, b), p;
+
+    p = _mm256_permutevar8x32_epi32(m, P2), m = _mm256_min_epi32(m, p);
+    p = _mm256_permutevar8x32_epi32(m, P4), m = _mm256_min_epi32(m, p);
+    p = _mm256_permutevar8x32_epi32(m, P8), m = _mm256_min_epi32(m, p);
+
+    uint32_t lo = _mm256_movemask_epi8(_mm256_cmpeq_epi32(a, m));
+    uint32_t hi = _mm256_movemask_epi8(_mm256_cmpeq_epi32(b, m));
+
+    size_t ci = __builtin_ctzll(((uint64_t)hi << 32) | lo) / 4;
+
+    return false;
+}
--- 16-ary_heap/heap.h
+++ 16-ary_heap/heap.h
@@ -1,1 +1,12 @@
 #include <inttypes.h>
+#include <stdbool.h>
+
+typedef struct minq_t {
+    int32_t * _a;
+    size_t _z, _c;
+} minq_t;
+
+__attribute__((warn_unused_result))
+bool minq_init(minq_t *q, size_t c);
+
+void minq_release(minq_t *q);
