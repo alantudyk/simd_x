b2b8c7c278f8132687456cfd2394ad49fa306d52
Reznov <yann.collet.is.not.a.perfectionist@gmail.com>
2024-04-23 12:39:54 +0300

    >>

--- 16-ary_heap/heap.h
+++ 16-ary_heap/heap.h
@@ -1,18 +1,18 @@
+#pragma once
+
 #include <inttypes.h>
 #include <stdbool.h>
 
 typedef struct minq_t {
-    int32_t * _a;
+    int32_t *_a;
     size_t _z, _c;
 } minq_t;
 
-__attribute__((warn_unused_result))
-bool minq_init(minq_t *q, size_t c);
+#define CHECK __attribute__((warn_unused_result))
 
+CHECK bool minq_init(minq_t *q, size_t c);
 void minq_release(minq_t *q);
+CHECK bool minq_push(minq_t *q, int32_t x);
+CHECK bool minq_pop(minq_t *q, int32_t *_x);
 
-__attribute__((warn_unused_result))
-bool minq_push(minq_t *q, int32_t x);
-
-__attribute__((warn_unused_result))
-bool minq_pop(minq_t *q, int32_t *_x);
+#undef CHECK
